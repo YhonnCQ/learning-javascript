3df8b5f8c8ff1b21480379479023ff28e47a22d3
YhonnCQ <yhonncq@gmail.com>
2020-04-14 14:01:04 -0500

    feat: JavaScript Everywhere
    Display Messages with JavaScript
    Add JavaScript to HTML
    Debug JavaScript in the Console
    JavaScript Practice Challenge

---
+++ javascript-basics/javascript-everywhere/js/script.js
@@ -0,0 +1,11 @@
+/*
+alert("Hello. Thanks for visiting!");
+console.log("Hello from the console.");
+document.write("<h1>Welcome to my web page.</h1>");
+
+alert("Hey, you're back for more?");
+*/
+alert("Hello, World!");
+console.log("Hello from the console.");
+alert("Tranks for visiting");
+document.write("<h1>Welcome to my web page.</h1>");
