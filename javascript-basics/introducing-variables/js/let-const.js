77e6b329e44833dfe91a0c016eebfeb2f7e032b7
YhonnCQ <yhonncq@gmail.com>
2020-04-15 05:33:45 -0500

    feat: Introducing Variables
    Create a Variable
    Naming Variables
    Change the Value in a Variable
    Define Variables with let and const
    Spacing and New Lines in JavaScript
    Update the Value of a Variable
    Practice Basic Variables, Input & Output in JavaScript

---
+++ javascript-basics/introducing-variables/js/let-const.js
@@ -0,0 +1,7 @@
+/*  Resources
+        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/let
+        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/const
+*/
+let score = 5;
+score += 10;
+console.log(score);
---
+++ javascript-basics/introducing-variables/js/var.js
@@ -0,0 +1,16 @@
+/*  Resources
+        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators#Addition
+        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Assignment_Operators#Addition_assignment_2
+*/
+var message = "Hello!";
+console.log(message);
+message = "Hello from JavaScript Basics";
+console.log(message);
+
+var score = 0;
+score += 10;
+score += 5;
+
+var bonusPts = 100;
+var finalScore = score + bonusPts;
+console.log(finalScore);
