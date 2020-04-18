092764ea1b4af1f553a2b3293cecd5e9ec0a0926
YhonnCQ <yhonncq@gmail.com>
2020-04-18 11:41:45 -0500

    feat: Working with Strings
    Introducing Strings
    Transform and Manipulate Strings
    Capture Input
    Combine Strings
    Template Literals
    Display the Value of a String on a Page
    Variables and Strings Challenge
    Practice Template Literals

---
+++ javascript-basics/introducing-strings/js/combine.js
@@ -0,0 +1,7 @@
+const name = prompt("What is your name?")
+let message = "Hello " + name + ". Welcome to my music site. ";
+message += "T'm so happy that you came by visit, ";
+message += name;
+message += ". Feel free to come again and listen to more music!";
+console.log(message);
+
---
+++ javascript-basics/introducing-strings/js/transform.js
@@ -0,0 +1,5 @@
+const passphrase = 'I Have Spoken';
+console.log(passphrase.length)
+console.log(passphrase.toUpperCase());
+console.log(passphrase.toLowerCase());
+console.log(passphrase);
